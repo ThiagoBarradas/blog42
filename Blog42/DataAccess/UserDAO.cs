386edc2459f3b1b983378072d749aa0f1ad827c1
Thiago Barradas <thiagobarradas@live.com>
2015-05-06 08:37:06 -0300

    Camada DAO para Usuário

---
+++ Blog42/DataAccess/UserDAO.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Blog42.Models;
+using Blog42.Helpers;
+
+namespace Blog42.DataAccess
+{
+    public class UserDAO
+    {
+        private Blog42Entities entities;
+
+        public UserDAO() { 
+            // Inicializa o modelo EF
+            entities = new Blog42Entities();
+        }
+
+        /*
+         * Método que verifica se o usuário existe, a senha está correta e é um usuário ativo.
+         * Se estiver tudo ok, retorna true, caso usuário ou senha incorretos ou usuário inativo, retorna false.
+         */
+        public bool AuthUser(String username, String password)
+        {
+            // transforma o password recebido em uma hash MD5 para comparar com a hash armazenada
+            password = CryptHelper.CryptPassword(password);
+
+            //Define a query de consulta para a combinação de usuário/senha e para usuários ativos
+            var Query = (from u in entities.User
+                         where u.Username.Equals(username, StringComparison.OrdinalIgnoreCase) && u.Password == password && u.IsActive
+                         select u).SingleOrDefault();
+
+            // Retorna true caso recupere um usuário ativo que o username e a senha combinem ou false caso contrário.
+            return (Query != null);
+        }
+
+        /*
+         * Método que retorna um usuário de acordo com seu username
+         * Caso não possua nenhum usuário com username, o retorno é null.
+         * 
+         */
+        public User getUser(string username)
+        {
+            // captura os dados do usuário e retorna
+            User user =  (from u in entities.User
+                         where u.Username == username
+                         select u).SingleOrDefault();
+            return user;
+        }
+
+        /*
+         * Método que retorna um usuário de acordo com seu id
+         * Caso não possua nenhum usuário com esse id, o retorno é null.
+         * 
+         */
+        public User getUser(int id)
+        {
+            // captura os dados do usuário e retorna
+            var user = entities.User.Find(id);
+            return user;
+        }
+
+        /*
+         * Cria um nova registro de usuário
+         */
+        public bool createUser(User user)
+        {
+            try
+            {
+                // Inseri e salva as alterações
+                entities.User.Add(user);
+                entities.SaveChanges();
+                // Se ocorrer tudo bem, retorna true
+                return true;
+            }
+            catch (Exception)
+            {
+                // Caso ocorra algum problema retorna false
+                return false;
+            }
+        }
+
+        /*
+         * Atualiza um registro de usuário
+         */
+        public bool updateUser(User user)
+        {
+            try
+            {
+                // recebe dados originais
+                var original = entities.User.Find(user.Id);
+
+                // se localizou registrou e recuperou dados, atualiza
+                if (original != null)
+                {
+                    // Atualiza dados
+                    original.Username = user.Username;
+                    original.Name = user.Name;
+                    original.Email = user.Email;
+                    original.Password = user.Password;
+                    original.IsActive = user.IsActive;
+                    original.IsAdmin = user.IsAdmin;
+                    original.IsFirstEntry = user.IsFirstEntry;
+                    entities.SaveChanges();
+                    // Se ocorrer tudo bem, retorna true
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                // Caso ocorra algum problema retorna false
+                return false;
+            }
+        }
+
+        /*
+         * Deleta um registro de usuário
+         */
+        public bool deleteUser(int id)
+        {
+            try
+            {
+                // recebe usuário 
+                var userSelected = entities.User.Find(id);
+                // remove usuário e salva
+                entities.User.Remove(userSelected);
+                entities.SaveChanges();
+                // Se ocorrer tudo bem, retorna true
+                return true;                
+            }
+            catch (Exception)
+            {
+                // Caso ocorra algum problema retorna false
+                return false;
+            }
+        }
+
+    }
+}
