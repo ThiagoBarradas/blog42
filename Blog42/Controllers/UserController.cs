6a119ec8ca2a6d684ce273cc705e3e689ccddafd
Thiago Barradas <thiagobarradas@live.com>
2015-05-06 21:23:55 -0300

    Reset de Senha no Primeiro Acesso

--- Blog42/Controllers/UserController.cs
+++ Blog42/Controllers/UserController.cs
@@ -7,11 +7,15 @@ using System.Web.Security;
 using Blog42.DataAccess;
 using Blog42.Models;
 using Blog42.Security;
+using Blog42.Helpers;
 
 namespace Blog42.Controllers
 {
     public class UserController : Controller
     {
+        // Cria objeto de acesso aos dados
+        private UserDAO userDAO = new UserDAO();
+
         //
         // GET: /Account/Login
         public ActionResult Login()
@@ -31,8 +35,6 @@ namespace Blog42.Controllers
         [ValidateInput(true)]
         public ActionResult Login(UserLogin user)
         {
-            // Cria objeto de acesso aos dados
-            UserDAO userDAO = new UserDAO();
             // Verifica se os dados para Login são válidos e autentica o usuário
             if (ModelState.IsValid && userDAO.AuthUser(user.Username, user.Password))
             {
@@ -101,6 +103,39 @@ namespace Blog42.Controllers
         [PermissionFilter(Roles = "FirstEntry")]
         public ActionResult ResetPassword(UserResetPassword passwords)
         {
+            // Recupera informações originais
+            User user = userDAO.getUser(System.Web.HttpContext.Current.User.Identity.Name);
+
+            // verifica se não conseguiu receber usuário
+            if (user == null)
+            {
+                // Desloga e redireciona para página de login
+                FormsAuthentication.SignOut();
+                return RedirectToAction("Login", "User");
+            }
+            else if(ModelState.IsValid) // Se recebeu usuário e dados são válidos
+            { 
+                // criptografa a nova senha                
+                string newpassword = CryptHelper.CryptPassword(passwords.Password);
+
+                // Se a nova senha for igual a antiga, retorna erro
+                if (newpassword == user.Password)
+                {
+                    ModelState.AddModelError("", "Você deve cadastrar uma senha diferente da atual.");
+                }
+                else
+                {
+                    // atribui nova senha e seta primeiro acesso como false
+                    user.Password = newpassword;
+                    user.IsFirstEntry = false;
+
+                    // tenta atualizar, se não conseguir informa erro 
+                    if (userDAO.updateUser(user))
+                        ViewBag.Success = true;
+                    else
+                        ModelState.AddModelError("", "Ops! Ocorreu um erro durante o processamento. Tente novamente.");
+                }
+            }            
             return View(passwords);
         }
 
